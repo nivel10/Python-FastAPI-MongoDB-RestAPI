2e4cfecd70c8004f8a9157850a4913e5fcd2a47b
T.S.U. - Ing. Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-20 16:27:13 -0400

    Sync - 2025/08/20 - 16:27
    Sync - 2025/08/20 - 16:27

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ from routes.user import user_router
 app = FastAPI(
     title='Python - FastAPI - MongoDB - restAPI',
     description='RestAPI testing',
-    version='1.0.0'
+    version='1.0.0',
+    openapi_tags=[
+        {'name':'users', 'description': 'user routers',},
+        {'name': 'main', 'description': 'main routers'}
+    ],
 )
 app.include_router(user_router)
 
 @app.get('/', tags=['main'])
+async def main():
+    return {'section': 'main'}
+
+@app.get('/hello', tags=['main'])
 async def hello():
-    return {'message': 'hello world'}
+    return {'section': 'main / hello'}
