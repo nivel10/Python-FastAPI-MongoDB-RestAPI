9d825e810a0ab01367bdd3d25ab581247955ec10
T.S.U. - Ing. Carlos H. <carlos.e.herrera.j@gmail.com>
2025-08-20 13:29:47 -0400

    Sync - 2025/08/20 - 13:29
    Sync - 2025/08/20 - 13:29

--- models/user.py
+++ models/user.py
@@ -1,6 +1,32 @@
 from pydantic import BaseModel
 from typing import Optional
 
+#region old code
+# class User_Base(BaseModel):
+#     name: str
+#     email: str
+
+# class User(User_Base, BaseModel):
+#     id: Optional[str]
+#     # name: str
+#     # email: str
+#     password: str
+
+# class User_DB(User_Base, BaseModel):
+#     _id: object
+#     # name: str
+#     # email: str
+#     password: str
+
+# # class User_Login():
+# #     email: str
+# #     password:str
+
+# #     def __init__(self, email: str, password: str):
+# #         self.email = email
+# #         self.password = password
+#endregion old code
+
 class User(BaseModel):
     id: Optional[str]
     name: str
@@ -13,14 +39,15 @@ class User_DB(BaseModel):
     email: str
     password: str
 
-# class User_Login():
-#     email: str
-#     password:str
-
-#     def __init__(self, email: str, password: str):
-#         self.email = email
-#         self.password = password
-
 class User_Login(BaseModel):
     email: str | None
     password: str | None
+
+class User_Not_Password(BaseModel):
+    id: Optional[str]
+    name: str
+    email: str
+
+class User_Before_After(BaseModel):
+    before: User
+    after: User
--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, status
 from config.db import conn, mongodb_collections
 from schemas.user import userEntity, usersEntity, userNotPasswordEntity
-from models.user import User, User_DB, User_Login
+from models.user import User, User_DB, User_Login, User_Not_Password, User_Before_After
 from bson import ObjectId
 from passlib.context import CryptContext
 # from passlib.hash import sha256_crypt
@@ -28,12 +28,21 @@ async def get_users():
 @user_router.post('/', response_model=User)
 async def create_user(user: User):
     try:
-        user_new = dict(user)
-        user_new.pop('id')
-        user_new['password'] = crypt.hash(user_new['password'])
+        #region old code
+        # user_new = dict(user)
+        # user_new.pop('id')
+        # user_new['password'] = crypt.hash(user_new['password'])
+        #endregion old code
+        user_new = user
+        del user_new.id
+        user_new.password = crypt.hash(user_new.password)
+        #region old code
         # # user_new['password'] = sha256_crypt.encrypt(user_new['password'])
         # user_new['password'] = sha256_crypt.hash(user_new['password'])
-        user_new_id = conn[mongodb_collections.users].insert_one(user_new).inserted_id
+        #endregion old code
+        # user_new_id = conn[mongodb_collections.users].insert_one(user_new).inserted_id
+        user_new_dict = dict(user_new)
+        user_new_id = conn[mongodb_collections.users].insert_one(user_new_dict).inserted_id
         user_new_found: User = await find_user(key='_id', value=ObjectId(str(user_new_id)))
 
         return user_new_found
@@ -55,10 +64,31 @@ async def get_user(id: str):
             detail=str(ex),
         )
 
-@user_router.put('/{id}')
-async def update_user(id, user):
+@user_router.put('/{id}', response_model=User_Before_After)
+async def update_user(id: str, user: User):
     try:
-        pass
+        user_before: User = await find_user('_id', ObjectId(id))
+        user_before = User(**user_before)
+        
+        user_data_updated = {
+            '$set': {
+                'name': user.name if user.name != '' else user_before.name,
+                'email': user.email if user.email != '' else user_before.email,
+                'password': crypt.hash(user.password) if user.password != user_before.password else user_before.password,
+            },
+        }
+
+        conn[mongodb_collections.users].update_one(
+            {'_id': ObjectId(id)},
+            update=user_data_updated,
+        )
+
+        user_after: User = await find_user(key='_id', value=ObjectId(id))
+
+        return {
+            'before': user_before,
+            'after': user_after,
+        }
     except Exception as ex:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -79,7 +109,7 @@ async def delete_user(id: str):
             detail=str(ex),
         )
 
-@user_router.post('/login')
+@user_router.post('/login', response_model= User_Not_Password)
 async def login_user(user_login: User_Login):
     try:
         if not user_login.email:
