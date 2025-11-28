69d4ea0b894f881ebca6552f629ce5f1fda2504a
T.S.U. - Ing. Carlos H <carlos.e.herrera.j@gmail.com>
2025-11-28 19:22:49 -0400

    Sync - 11/28/2025 - 19:22

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from routes.user import user_router
+from fastapi.middleware.cors import CORSMiddleware
+
+ENV_STATE = os.getenv('ENV_STATE', '')
+
+origins = [
+    '*'
+]
 
 app = FastAPI(
     title='Python - FastAPI - MongoDB - restAPI',
@@ -10,6 +19,16 @@ app = FastAPI(
         {'name': 'main', 'description': 'main routers'}
     ],
 )
+
+app.add_middleware(
+     CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
+
 app.include_router(user_router)
 
 @app.get('/', tags=['main'])
@@ -19,3 +38,13 @@ async def main():
 @app.get('/hello', tags=['main'])
 async def hello():
     return {'section': 'main / hello'}
+
+if __name__ == '__main__':
+    if ENV_STATE == 'dev':
+        uvicorn.run(
+            'main:app',
+            host='127.0.0.1',
+            port=8000,
+            reload=True,
+            log_level='debug',
+        )
